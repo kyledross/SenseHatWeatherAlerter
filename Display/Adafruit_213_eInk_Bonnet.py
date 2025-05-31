efc3b9bcf3b6e5e36ad5bb4509f94a65154411d9
Kyle D. Ross <6475128+kyledross@users.noreply.github.com>
2025-05-31 16:54:34 -0400

    Update eInk display implementation and configuration
    - Modify `Adafruit213eInkBonnet` to include custom font handling via `font5x8.bin`.
    - Add `os.path` to dynamically load font path relative to the script location.
    - Enhance `display_message` and `clear_display` methods for improved functionality.
    - Replace `clearScreen` with `clear_display` for naming consistency.
    - Update `config.txt` to reflect new display type and settings.
    - Include `font5x8.bin` binary file for font data.

--- Display/Adafruit_213_eInk_Bonnet.py
+++ Display/Adafruit_213_eInk_Bonnet.py
@@ -2,6 +2,7 @@ from abc import ABC
 import board
 import busio
 import digitalio
+import os.path
 from adafruit_epd.epd import Adafruit_EPD
 
 from Display.IDisplay import IDisplay
@@ -9,18 +10,20 @@ from Display.IDisplay import IDisplay
 # SD1680 version, not 1680Z
 class Adafruit213eInkBonnet(IDisplay, ABC):
     def __init__(self):
-
         spi = busio.SPI(board.SCK, MOSI=board.MOSI, MISO=board.MISO)
         ecs = digitalio.DigitalInOut(board.CE0)
         dc = digitalio.DigitalInOut(board.D22)
         rst = digitalio.DigitalInOut(board.D27)
         busy = digitalio.DigitalInOut(board.D17)
         srcs = None
+        current_dir = os.path.dirname(os.path.abspath(__file__))
+        self.font_path = os.path.join(current_dir, 'font5x8.bin')
 
         from adafruit_epd.ssd1680 import Adafruit_SSD1680
         self.display = Adafruit_SSD1680(122, 250, spi, cs_pin=ecs, dc_pin=dc, sramcs_pin=None,
                                         rst_pin=rst, busy_pin=busy)
         self.display.rotation = 3
+        
 
     def detail(self, detail: str):
         pass
@@ -46,16 +49,17 @@ class Adafruit213eInkBonnet(IDisplay, ABC):
         return lines
 
     def display_message(self, title: str, message: str = "", detail: str = "", color=None):
-        self.clearScreen()
-        self.display.text(title, 10, 10, Adafruit_EPD.BLACK, size=3)
-        self.display.text(message, 10, 40, Adafruit_EPD.BLACK, size=2)
+        self.clear_display()
+        self.display.text(title, 10, 10, Adafruit_EPD.BLACK, size=3, font_name=self.font_path)
+        self.display.text(message, 10, 40, Adafruit_EPD.BLACK, size=2, font_name=self.font_path)
         # Calculate lines needed for detail text with word wrapping
         lines = self._wrap_text(detail, 40)
         for i, line in enumerate(lines):
-            self.display.text(line, 10, 60 + (i * 10), Adafruit_EPD.BLACK, size=1)
+            self.display.text(line, 10, 60 + (i * 10), Adafruit_EPD.BLACK, size=1, font_name=self.font_path)
         self.display.display()
         pass
 
-    def clearScreen(self):
+    def clear_display(self):
         self.display.fill(Adafruit_EPD.WHITE)
+        self.display.display()
         pass
