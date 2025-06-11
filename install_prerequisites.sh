7db6deafb3e711d896894e6951a4507e7c6cde63
Kyle D. Ross <6475128+kyledross@users.noreply.github.com>
2025-06-10 21:19:53 -0400

    Streamline `install_prerequisites.sh` with GPIO support and non-interactive script execution.

--- install_prerequisites.sh
+++ install_prerequisites.sh
@@ -24,17 +24,20 @@ sudo apt install sense-hat -y
 pip3 install adafruit-circuitpython-epd
 pip3 install Pillow
 
+# Install GPIO support
+pip3 install RPi.GPIO
+
 # Install Blinka
 pip3 install --upgrade adafruit-python-shell
 wget https://raw.githubusercontent.com/adafruit/Raspberry-Pi-Installer-Scripts/master/raspi-blinka.py
 # todo See if this can exit without prompting for reboot
-sudo -E env PATH=$PATH python3 raspi-blinka.py
+sudo -E env PATH=$PATH python3 raspi-blinka.py <<< 'n'
 
 # Disable SPI Chip Enable Lines
 pip3 install --upgrade adafruit-python-shell click
 wget https://raw.githubusercontent.com/adafruit/Raspberry-Pi-Installer-Scripts/main/raspi-spi-reassign.py
 # todo See if this can exit without prompting for reboot
-sudo -E env PATH=$PATH python3 raspi-spi-reassign.py --ce0=disabled --ce1=disabled
+sudo -E env PATH=$PATH python3 raspi-spi-reassign.py --ce0=disabled --ce1=disabled <<< 'n'
 
 # Reboot
 echo "Press Enter to reboot or Ctrl+C to cancel..."
