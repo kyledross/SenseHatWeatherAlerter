5bb3544753c600d58019b7db4e8f44809cc36a89
Kyle D. Ross <6475128+kyledross@users.noreply.github.com>
2025-06-10 21:08:04 -0400

    Expand `install_prerequisites.sh` with additional dependencies and placeholders; add stubbed `heartbeat` method to `ConsoleDisplay`.

--- install_prerequisites.sh
+++ install_prerequisites.sh
@@ -5,8 +5,11 @@
 # USE AT YOUR OWN RISK
 #
 
+# Basic requirements
+sudo apt install python3-dev build-essential -y
 
 # Create virtual environment -----------------------
+sudo apt install python3-venv -y
 python -m venv .venv
 source .venv/bin/activate
 
@@ -14,23 +17,23 @@ source .venv/bin/activate
 pip install -r requirements.txt
 
 # Install SenseHat support -------------------------
-sudo apt install sense-hat
+sudo apt install sense-hat -y
 
 # Install eInk Bonnet support ----------------------
 # Install Circuit Python
-sudo apt install python3-venv
-source .venv/bin/activate
 pip3 install adafruit-circuitpython-epd
 pip3 install Pillow
 
 # Install Blinka
 pip3 install --upgrade adafruit-python-shell
 wget https://raw.githubusercontent.com/adafruit/Raspberry-Pi-Installer-Scripts/master/raspi-blinka.py
+# todo See if this can exit without prompting for reboot
 sudo -E env PATH=$PATH python3 raspi-blinka.py
 
 # Disable SPI Chip Enable Lines
 pip3 install --upgrade adafruit-python-shell click
 wget https://raw.githubusercontent.com/adafruit/Raspberry-Pi-Installer-Scripts/main/raspi-spi-reassign.py
+# todo See if this can exit without prompting for reboot
 sudo -E env PATH=$PATH python3 raspi-spi-reassign.py --ce0=disabled --ce1=disabled
 
 # Reboot
